310a4a5c201df19516b2c4fefd0e289be18a8db9
mmc <cieplakm@gmail.com>
2021-05-25 17:58:27 +0200

    Fix CertPlay

--- src/main/java/com/mmc/playground/security/CertificatePlay.java
+++ src/main/java/com/mmc/playground/security/CertificatePlay.java
@@ -1,49 +1,44 @@
 package com.mmc.playground.security;
 
-import sun.security.x509.AlgorithmId;
-import sun.security.x509.CertificateAlgorithmId;
-import sun.security.x509.CertificateSerialNumber;
-import sun.security.x509.CertificateValidity;
-import sun.security.x509.CertificateVersion;
-import sun.security.x509.CertificateX509Key;
-import sun.security.x509.X500Name;
-import sun.security.x509.X509CertImpl;
-import sun.security.x509.X509CertInfo;
+import sun.security.x509.*;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.math.BigInteger;
-import java.security.InvalidKeyException;
-import java.security.KeyPair;
-import java.security.KeyPairGenerator;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.NoSuchProviderException;
-import java.security.PrivateKey;
-import java.security.PublicKey;
-import java.security.SecureRandom;
-import java.security.SignatureException;
-import java.security.UnrecoverableEntryException;
+import java.security.*;
 import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
+import java.util.Base64;
 import java.util.Date;
 
+
+/**
+ * Must have: JDK8 (not above)!
+ */
 public class CertificatePlay {
     public static void main(String[] args) throws CertificateException, NoSuchAlgorithmException, IOException, KeyStoreException, SignatureException, NoSuchProviderException, InvalidKeyException, UnrecoverableEntryException {
-        KeyStore keyStore = CertificatePlay.loadKeyStore("s4.jks", "ksPass");
-        KeyStore.PrivateKeyEntry privateKeyEntry = (KeyStore.PrivateKeyEntry) keyStore.getEntry("cert", new KeyStore.PasswordProtection("pkPass".toCharArray()));
+        String keyStoreFileName = "cert.jks";
+        String keyStorePass = "pass";
+        String privateKeyPass = "pkPass";
+        String alias = "myCert";
+        String subjectName = "MySubject";
+        String issuerName = "MyIssuerName";
+
+        KeyStore keyStore = CertificatePlay.loadKeyStore(keyStoreFileName, keyStorePass);
+        KeyStore.PrivateKeyEntry privateKeyEntry = (KeyStore.PrivateKeyEntry) keyStore.getEntry(alias, new KeyStore.PasswordProtection(privateKeyPass.toCharArray()));
         PrivateKey privateKey = privateKeyEntry.getPrivateKey();
+
+        String s = Base64.getEncoder().encodeToString(privateKey.getEncoded());
+        System.out.println(s);
 
         KeyPairGenerator keyGenerator = KeyPairGenerator.getInstance("RSA");
         KeyPair keyPair = keyGenerator.generateKeyPair();
 
-        String subjectName = "s5";
-        String issuerName = "s4";
-        Certificate certificate = createCertificate(subjectName, issuerName, keyPair.getPublic(), privateKey);
-        writeKeyStore("cert", subjectName + ".jks", "ksPass", "pkPass", keyPair.getPrivate(), certificate);
+        Certificate certificate = createCertificate(subjectName, issuerName, keyPair.getPublic(), keyPair.getPrivate());
+        writeKeyStore(alias, keyStoreFileName, keyStorePass, privateKeyPass, keyPair.getPrivate(), certificate);
     }
+
 
     static void writeKeyStore(String alias, String fileNameWithExtension, String keyStorePassword, String privateKeyPassword, PrivateKey privateKey, Certificate certificate) throws CertificateException, IOException, NoSuchAlgorithmException, NoSuchProviderException, InvalidKeyException, SignatureException, KeyStoreException {
         KeyStore keyStore = KeyStore.getInstance("JKS");
@@ -66,13 +61,13 @@ public class CertificatePlay {
 
         X509CertInfo certInfo = new X509CertInfo();
 
-        AlgorithmId algo = new AlgorithmId(AlgorithmId.sha256WithRSAEncryption_oid);
+        AlgorithmId algorithmId = new AlgorithmId(AlgorithmId.sha256WithRSAEncryption_oid);
 
         X500Name issuer = new X500Name("CN=" + issuerName);
         X500Name subject = new X500Name("CN=" + subjectName);
         certInfo.set(X509CertInfo.VERSION, new CertificateVersion(CertificateVersion.V3));
         certInfo.set(X509CertInfo.SERIAL_NUMBER, new CertificateSerialNumber(serialNumber));
-        certInfo.set(X509CertInfo.ALGORITHM_ID, new CertificateAlgorithmId(algo));
+        certInfo.set(X509CertInfo.ALGORITHM_ID, new CertificateAlgorithmId(algorithmId));
         certInfo.set(X509CertInfo.SUBJECT, subject);
         certInfo.set(X509CertInfo.ISSUER, issuer);
         certInfo.set(X509CertInfo.KEY, new CertificateX509Key(publicKey));

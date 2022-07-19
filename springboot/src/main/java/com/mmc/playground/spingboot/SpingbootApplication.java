24b2216b1e66bd3a5dd231775248607a36857cd2
mmc <cieplakm@gmail.com>
2022-07-19 22:01:18 +0200

    Ad springboot basics

---
+++ springboot/src/main/java/com/mmc/playground/spingboot/SpingbootApplication.java
@@ -0,0 +1,12 @@
+package com.mmc.playground.spingboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingbootApplication.class, args);
+	}
+}
---
+++ springboot/src/test/java/com/mmc/playground/spingboot/SpingbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmc.playground.spingboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

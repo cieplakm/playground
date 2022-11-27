75806e4e10fffae865a943f16e7cb221eb42808d
mmc <cieplakm@gmail.com>
2022-11-27 20:40:09 +0100

    Fix compilation

--- hexagonal/module/application/src/integration-test/java/com/mmc/playground/ContainerizedMongoDbTest.java
+++ hexagonal/module/application/src/integration-test/java/com/mmc/playground/ContainerizedMongoDbTest.java
@@ -16,7 +16,7 @@ import static org.junit.jupiter.api.extension.ExtensionContext.Namespace.GLOBAL;
 public class ContainerizedMongoDbTest {
 
     @Container
-    public static MongoDBContainer MONGO_DB_CONTAINER = new MongoDBContainer(DockerImageName.parse("mongo:5.0.13"))
+    public static MongoDBContainer MONGO_DB_CONTAINER = new MongoDBContainer("mongo:5.0.13")
             .withNetworkMode("bridge")
             .withReuse(true);
 

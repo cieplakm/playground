54fd4991a5f3ee00147333965c0f90edc714eebd
mmc <cieplakm@gmail.com>
2022-12-02 19:12:35 +0100

    Remove useless test, Use value from properties in tests

--- hexagonal/module/application/src/integration-test/java/com/mmc/playground/userprofile/UserProfileDbIT.java
+++ hexagonal/module/application/src/integration-test/java/com/mmc/playground/userprofile/UserProfileDbIT.java
@@ -5,22 +5,28 @@ import com.mmc.playground.ContainerizedMongoDbTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @ContextConfiguration(classes = {Application.class})
+@TestPropertySource(value = "classpath:application-test.properties")
 public class UserProfileDbIT extends ContainerizedMongoDbTest {
 
     @Autowired
     UserProfileRepository userProfileRepository;
 
+    @Value("${test.name}")
+    String testName;
+
     @Test
     public void should() {
-        String s = userProfileRepository.create(new UserProfile("Mati", "C", "AK50"));
+        String s = userProfileRepository.create(new UserProfile(testName, "C", "AK50"));
 
         UserProfile userProfile = userProfileRepository.fetchById(s);
 
-        Assertions.assertEquals(userProfile.getName(), "Mati");
+        Assertions.assertEquals(userProfile.getName(), testName);
     }
 }

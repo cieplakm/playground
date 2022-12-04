63fa6ced9032168c77b2ac0ed42d79e3359435d9
mmc <cieplakm@gmail.com>
2022-12-04 22:51:57 +0100

    Move test to integration tests

--- hexagonal/module/application/src/integration-test/java/com/mmc/playground/liveness/LivenessControllerTest.java
+++ hexagonal/module/application/src/integration-test/java/com/mmc/playground/liveness/LivenessControllerTest.java
@@ -6,11 +6,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ContextConfiguration(classes = {TestApplication.class})
+@TestPropertySource(value = "classpath:application-test.properties")
 public class LivenessControllerTest {
 
     @LocalServerPort
